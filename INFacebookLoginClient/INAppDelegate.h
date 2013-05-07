368350f4ae69b7223cd3ccd643ce3d273341a949
Indragie Karunaratne <i@indragie.com>
2013-05-06 22:03:59 -0700

    Initial Commit

---
+++ INFacebookLoginClient/INAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  INAppDelegate.h
+//  INFacebookLoginClient
+//
+//  Created by Indragie Karunaratne on 2013-05-06.
+//  Copyright (c) 2013 indragie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface INAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ INFacebookLoginClient/INAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  INAppDelegate.m
+//  INFacebookLoginClient
+//
+//  Created by Indragie Karunaratne on 2013-05-06.
+//  Copyright (c) 2013 indragie. All rights reserved.
+//
+
+#import "INAppDelegate.h"
+
+@implementation INAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
